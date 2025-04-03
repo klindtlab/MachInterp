804438de1984433b50f80bcd5eef4278e16c4896
David Klindt <david@MacBookPro.cshl.edu>
2025-04-02 20:38:38 -0400

    tests for lazy dreamsim

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -8,8 +8,15 @@ print(f"Using device: {device}")
 
 # Test images
 size = 32
+torch.manual_seed(97823)
+np.random.seed(97823)
 img_a = np.random.uniform(-1, 1, (2, 3, size, size))
 img_b = np.random.uniform(-1, 1, (3, 3, size, size))
+label_a = torch.randint(0, 1000, img_a.shape[:1])
+label_b = torch.randint(0, 1000, img_b.shape[:1])
+dataset = np.random.uniform(-1, 1, (42, 3, size, size))
+ind_a = torch.randint(0, 42, (2,))
+ind_b = torch.randint(0, 42, (3,))
 
 print('\n\n')
 print('Test LPIPS')
@@ -44,8 +51,13 @@ print(f"Perceptual similarity (should be symmetric and 0 on diagonal): {metric.C
 
 print('\n\n')
 print("Test LabelMetric")
-label_a = torch.randint(0, 1000, img_a.shape[:1])
-label_b = torch.randint(0, 1000, img_b.shape[:1])
 print(label_a, label_b)
 print("Output shape (should be 2 x 3): ", metric.LabelMetric().compute_similarity(label_a, label_b).shape)
 print(f"Perceptual similarity (should be symmetric and 0 on diagonal): {metric.LabelMetric().compute_similarity(label_a, label_a)}")
+
+print('\n\n')
+print('Test LazyDreamSimMetric')
+dreamsim_metric = metric.LazyDreamSimMetric(dataset, device)
+print("Output shape (should be 2 x 3): ", dreamsim_metric.compute_similarity(ind_a, ind_b).shape)
+print(f"Perceptual similarity (should be symmetric and 0 on diagonal): {dreamsim_metric.compute_similarity(ind_a, ind_a)}")
+print(f"Same result? (i.e. test for deterministic): {metric.LazyDreamSimMetric(dataset, device).compute_similarity(ind_a, ind_a)}")
